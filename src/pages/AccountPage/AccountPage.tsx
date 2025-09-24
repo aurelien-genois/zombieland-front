4c96dfb12c47e400fb05fc6b5cdaa4b508e7be78
aurelien-genois <aurelien.genois@gmail.com>
2025-09-24 10:36:32 +0200

    ♻️ refactor(app): Account page: intermediate function "updateActiveTab" has props for AccountTabs
    Changelog: refactor

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -6,14 +6,20 @@ import OrdersHistory from "./OrdersHistory";
 export default function AccountPage() {
   const [activeTab, setActiveTab] = useState("infos");
 
-  return(
+  function updateActiveTab(tab: string): void {
+    setActiveTab(tab);
+  }
+
+  return (
     <>
-      <div className='bg-gray-300 pt-16 sm:pt-20  min-h-[calc(100svh-5rem-1.45rem)]' >
-        <h1 className="text-center font-bold text-3xl pt-10 pb-8">Mon compte</h1>
-        <AccountTabs activeTab={activeTab} setActiveTab={setActiveTab}/>
+      <div className="bg-gray-300 pt-16 sm:pt-20  min-h-[calc(100svh-5rem-1.45rem)]">
+        <h1 className="text-center font-bold text-3xl pt-10 pb-8">
+          Mon compte
+        </h1>
+        <AccountTabs activeTab={activeTab} updateActiveTab={updateActiveTab} />
         {activeTab === "infos" && <InfosUpdate />}
         {activeTab === "orders" && <OrdersHistory />}
       </div>
     </>
-  )
+  );
 }
--- src/pages/AccountPage/AccoutTabs.tsx
+++ src/pages/AccountPage/AccoutTabs.tsx
@@ -1,10 +1,16 @@
-export default function AccountTabs({activeTab, setActiveTab}) {
+interface AccountTabsProps {
+  activeTab: string;
+  updateActiveTab: (tab: string) => void;
+}
+
+
+export default function AccountTabs({activeTab, updateActiveTab}: AccountTabsProps) {
   return(
     <>
       <div className="flex justify-center text-center">
-          <button onClick={() => setActiveTab("infos")} className={`w-70 mb-5 text-center text-2xl sm:text-xl
+          <button onClick={() => updateActiveTab("infos")} className={`w-70 mb-5 text-center text-2xl sm:text-xl
             ${activeTab === "infos" ? "border-b-2 border-red-500 font-bold" : "cursor-pointer"}`}>Informations personnelles</button>
-          <button onClick={() => setActiveTab("orders")} className={`w-70 mb-5 text-2xl sm:text-xl
+          <button onClick={() => updateActiveTab("orders")} className={`w-70 mb-5 text-2xl sm:text-xl
             ${activeTab === "orders" ? "border-b-2 border-red-500 font-bold" : "cursor-pointer"}`}>Historique des commandes</button>
       </div>
     </>
