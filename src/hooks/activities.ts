12e7a584a047f5c3a9b7ccb993de03ef72fce5f8
aurelien-genois <aurelien.genois@gmail.com>
2025-09-26 11:50:34 +0200

    🔨 modify(app): Home Activities: add customHook useActivities
    Changelog: modify

--- src/hooks/activities.ts
+++ src/hooks/activities.ts
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "./redux";
-import { fetchOneActivity } from "../store/reducers/activitiesReducer";
+import {
+  fetchOneActivity,
+  fetchActivities,
+} from "../store/reducers/activitiesReducer";
 
 export function useActivity(slug: string) {
   const dispatch = useAppDispatch();
@@ -13,4 +16,21 @@ export function useActivity(slug: string) {
   }, [dispatch, slug]);
 
   return { currentActivity, loading, error };
+}
+
+interface IActivitiesFilters {
+  perPage?: number;
+}
+
+export function useActivities({ perPage = 20 }: IActivitiesFilters) {
+  const dispatch = useAppDispatch();
+  const { activities, loading, error } = useAppSelector(
+    (state) => state.activitiesStore
+  );
+
+  useEffect(() => {
+    dispatch(fetchActivities({ perPage }));
+  }, [dispatch, perPage]);
+
+  return { activities, loading, error };
 }
