ec3056fead283874473dcb92db90d1e7983437d9
MickSDonovan <michel.colacino@gmail.com>
2025-09-26 09:06:39 +0100

    ♻️ refactor(app): Changing branch push
    Changelog: refactor

--- src/pages/BackOffice/UsersManagement/UsersManagment.tsx
+++ src/pages/BackOffice/UsersManagement/UsersManagment.tsx
@@ -2,7 +2,7 @@ export default function UsersManagement() {
   return (
     <div>
       <h1>Users Management</h1>
-      <p>Manage your users here.</p>
+      <p>Manage your users here...</p>
     </div>
   );
 }
