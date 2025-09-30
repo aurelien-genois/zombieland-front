cec23689dcb9df85527366385ee4a8864e17b4f8
aurelien-genois <aurelien.genois@gmail.com>
2025-09-30 10:14:04 +0200

    🐛 fix(app): Home activities: ajustments on carousel
    Changelog: fix

--- src/pages/FrontOffice/_HomePage/Carousel.tsx
+++ src/pages/FrontOffice/_HomePage/Carousel.tsx
@@ -33,8 +33,8 @@ export default function Carousel() {
             "hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-blue-500 dark:hs-carousel-active:border-blue-500",
           mode: "default",
           isAutoHeight: false,
-          isAutoPlay: false,
-          isCentered: true,
+          isAutoPlay: true,
+          isCentered: false,
           isDraggable: true,
           isInfiniteLoop: true,
           isRTL: false,
@@ -45,7 +45,7 @@ export default function Carousel() {
             md: 2,
             lg: 3,
           },
-          speed: 6000,
+          speed: 3000,
           updateDelay: 4,
         }
       );
@@ -71,7 +71,7 @@ export default function Carousel() {
 
   return (
     <div ref={carouselRef} className="relative hs-carousel">
-      <div className="hs-carousel w-full overflow-hidden bg-black-bg-carousel rounded-lg dark:bg-neutral-900 pt-5 ">
+      <div className="hs-carousel w-full overflow-hidden bg-black-bg-carousel rounded-lg dark:bg-neutral-900 pt-5 px-8 ">
         <div className="relative min-h-110 -mx-1">
           <div className="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap opacity-0 transition-transform duration-700">
             {activities.map((activity) => (
@@ -96,7 +96,7 @@ export default function Carousel() {
 
       <button
         type="button"
-        className="hs-carousel-prev cursor-pointer hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-11.5 h-full text-gray-800 hover:bg-gray-800/10 focus:outline-hidden focus:bg-gray-800/10 rounded-s-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+        className="hs-carousel-prev cursor-pointer hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-11.5 h-full text-gray-800 bg-red-800 hover:bg-gray-800/10 focus:outline-hidden focus:bg-gray-800/10 rounded-s-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
       >
         <span className="text-2xl" aria-hidden="true">
           <svg
@@ -118,7 +118,7 @@ export default function Carousel() {
       </button>
       <button
         type="button"
-        className="hs-carousel-next cursor-pointer hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-11.5 h-full text-gray-800 hover:bg-gray-800/10 focus:outline-hidden focus:bg-gray-800/10 rounded-e-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+        className="hs-carousel-next cursor-pointer hs-carousel-disabled:opacity-50 hs-carousel-disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-11.5 h-full text-gray-800 bg-red-800 hover:bg-gray-800/10 focus:outline-hidden focus:bg-gray-800/10 rounded-e-lg dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
       >
         <span className="sr-only">Next</span>
         <span className="text-2xl" aria-hidden="true">
@@ -138,8 +138,6 @@ export default function Carousel() {
           </svg>
         </span>
       </button>
-
-      <div className="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 gap-x-2"></div>
     </div>
   );
 }
