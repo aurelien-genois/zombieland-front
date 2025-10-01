fa78ec9ddcde5672ea69806d9c3a1941269f0967
aurelien-genois <aurelien.genois@gmail.com>
2025-10-01 15:37:54 +0200

    🔨 modify(app): Admin Button component: use for submit button in form
    Changelog: modify

--- src/components/UI/BackOffice/Button.tsx
+++ src/components/UI/BackOffice/Button.tsx
@@ -7,6 +7,8 @@ interface IButtonProps {
   color?: "red" | "blue" | "green" | "gray";
   additionalClasses?: string;
   type?: "button" | "reset" | "submit" | "router-link";
+  name?: string;
+  disabled?: boolean;
 }
 
 export default function Button({
@@ -16,14 +18,17 @@ export default function Button({
   color = "blue",
   additionalClasses = "",
   type = "button",
+  name = "",
+  disabled = false,
 }: IButtonProps) {
   const baseStyle =
     "cursor-pointer px-3 py-1 border text-xs font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors";
 
   const colorStyles = {
-    red: "text-red-700 bg-red-100 border-red-700 hover:bg-red-200  focus:ring-red-500",
+    red: "text-red-700 bg-red-100 border-red-700 hover:bg-red-200 focus:ring-red-500",
     blue: "text-white bg-blue-500 border-blue-900 hover:bg-cyan-400 focus:ring-blue-500",
-    green: "",
+    green:
+      "text-green-700 bg-green-100 border-green-700 hover:bg-green-200 focus:ring-green-500",
     gray: "text-white bg-gray-700 border-gray-900 hover:bg-gray-500 focus:ring-gray-700",
   };
 
@@ -46,6 +51,8 @@ export default function Button({
     return (
       <button
         type={type}
+        name={name}
+        disabled={disabled}
         className={
           baseStyle + " " + colorStyles[color] + " " + additionalClasses
         }
