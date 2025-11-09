1b1e20a54abf5d5d24a37b869a534ba5c97237fb
aurelien.genois <aurelien.genois@gmail.com>
2025-11-09 15:08:49 +0100

    🐛 fix(app): pagination interval
    Changelog: fix

--- src/components/UI/Pagination.tsx
+++ src/components/UI/Pagination.tsx
@@ -23,6 +23,7 @@ export default function Pagination({
 
   let pagesArr = Array.from({ length: nbPages }, (_, i) => i + 1);
   pagesArr = pagesArr.filter((page) => {
+    page = Number(page);
     return (
       (page < currentPage + 2 && page > currentPage - 2) ||
       page > nbPages - 2 ||
@@ -30,23 +31,20 @@ export default function Pagination({
     );
   });
 
-  return (
-    <nav className="mt-8 flex items-center justify-center gap-2 sm:gap-3">
-      <button
-        className="size-9 group grid place-items-center cursor-pointer bg-grey-menu rounded-full border border-transparent hover:bg-white/10 hover:border-black disabled:pointer-events-none disabled:opacity-0"
-        onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : 1)}
-        disabled={currentPage === 1}
-      >
-        <span
-          aria-hidden
-          className="size-4 bg-white group-[:hover]:bg-black [mask:url(/src/assets/icon/fleche-droite.svg)_no-repeat_center/contain] rotate-180"
-        />
-      </button>
+  const pagesArrDisplayed: Array<string | number> = [];
+  for (let i = 0; i < pagesArr.length; i++) {
+    const currP = Number(pagesArr[i]);
+    pagesArrDisplayed.push(currP); // get page number
+    if (pagesArr[i + 1] && currP + 1 !== pagesArr[i + 1]) {
+      pagesArrDisplayed.push("..."); // if next page exist & is more than current+1, insert interval
+    }
+  }
 
-      {pagesArr.map((page, index) => {
-        if (pagesArr[index + 1] && page + 1 !== pagesArr[index + 1]) {
-          return <span key={page + 1}>...</span>;
-        } else {
+  return (
+    <nav className="mt-8 flex items-center justify-between gap-2 sm:gap-3">
+      <div>
+        {pagesArrDisplayed.map((page, index) => {
+          if (page === "...") return <span key={`...${index}`}>...</span>;
           return (
             <button
               key={page}
@@ -55,26 +53,39 @@ export default function Pagination({
                   ? "bg-brand text-xl text-blue-900"
                   : "hover:text-blue-700 cursor-pointer text-gray-500"
               }`}
-              onClick={() => setCurrentPage(page)}
+              onClick={() => setCurrentPage(Number(page))}
             >
               {page}
             </button>
           );
-        }
-      })}
+        })}
+      </div>
 
-      <button
-        className="group size-9 grid place-items-center cursor-pointer bg-grey-menu rounded-full border border-transparent hover:bg-white/10 hover:border-black disabled:pointer-events-none disabled:opacity-0"
-        onClick={() =>
-          setCurrentPage(currentPage < nbPages ? currentPage + 1 : nbPages)
-        }
-        disabled={currentPage === nbPages}
-      >
-        <span
-          aria-hidden
-          className="size-4 bg-white group-[:hover]:bg-black [mask:url(/src/assets/icon/fleche-droite.svg)_no-repeat_center/contain]"
-        />
-      </button>
+      <div className="flex gap-2">
+        <button
+          className="size-9 group grid place-items-center cursor-pointer bg-grey-menu rounded-full border border-transparent hover:bg-white/10 hover:border-black disabled:pointer-events-none disabled:opacity-0"
+          onClick={() => setCurrentPage(currentPage > 1 ? currentPage - 1 : 1)}
+          disabled={currentPage === 1}
+        >
+          <span
+            aria-hidden
+            className="size-4 bg-white group-[:hover]:bg-black [mask:url(/src/assets/icon/fleche-droite.svg)_no-repeat_center/contain] rotate-180"
+          />
+        </button>
+
+        <button
+          className="group size-9 grid place-items-center cursor-pointer bg-grey-menu rounded-full border border-transparent hover:bg-white/10 hover:border-black disabled:pointer-events-none disabled:opacity-0"
+          onClick={() =>
+            setCurrentPage(currentPage < nbPages ? currentPage + 1 : nbPages)
+          }
+          disabled={currentPage === nbPages}
+        >
+          <span
+            aria-hidden
+            className="size-4 bg-white group-[:hover]:bg-black [mask:url(/src/assets/icon/fleche-droite.svg)_no-repeat_center/contain]"
+          />
+        </button>
+      </div>
     </nav>
   );
 }
